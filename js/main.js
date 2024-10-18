68afdc3077eb12225492234f12eb6bd79ecfb04a
Dmitry Farino <dfarino46@gmail.com>
2024-10-19 02:27:02 +0300

    Ourtouch done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const reviews = document.querySelectorAll('.review');
 const reviewsBg = document.querySelectorAll('.review-bg');
+const reviews2 = document.querySelectorAll('.review2');
+const reviews2Bg = document.querySelectorAll('.review2-bg');
 
 reviews.forEach((review, index) => {
   const fullBtn = review.querySelector('.review__full-btn');
@@ -18,6 +20,32 @@ reviews.forEach((review, index) => {
       bg.parentElement.getBoundingClientRect().top;
     let reviewWidth = review.getBoundingClientRect().width;
     let reviewHeight = review.getBoundingClientRect().height;
+
+    bg.style.left = reviewX + reviewWidth / 2 + 'px';
+    bg.style.top = reviewY + reviewHeight / 2 + 'px';
+    
+  };
+  bgPos();
+  window.addEventListener('resize', bgPos);
+});
+
+reviews2.forEach((review2, index) => {
+  const fullBtn = review2.querySelector('.review2__full-btn');
+  const bg = reviews2Bg[index];
+
+  fullBtn.addEventListener('click', () => {
+    review2.classList.toggle('_full');
+  });
+
+  const bgPos = () => {
+    let reviewX = 
+      review2.getBoundingClientRect().left -
+      bg.parentElement.getBoundingClientRect().left;
+    let reviewY = 
+      review2.getBoundingClientRect().top -
+      bg.parentElement.getBoundingClientRect().top;
+    let reviewWidth = review2.getBoundingClientRect().width;
+    let reviewHeight = review2.getBoundingClientRect().height;
 
     bg.style.left = reviewX + reviewWidth / 2 + 'px';
     bg.style.top = reviewY + reviewHeight / 2 + 'px';
